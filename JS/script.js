09cd71be22039dc221c537e11420d91dd1158284
EngDann <137555908+EngDann@users.noreply.github.com>
2024-04-26 16:18:07 -0300

    Documentando script.js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,14 @@
+// Chave de API para acessar o serviço de dados meteorológicos OpenWeatherMap.
 const apiKey = "ba605efc18f1572f61892fe426f18a1a";
+
+// Seleciona elementos do DOM para interagir com a entrada do usuário, botão de busca, e outros elementos da UI.
 const city_input = document.querySelector("#city-input");
 const search_btn = document.querySelector("#search");
 const city_ = document.querySelector("#city_");
 const container = document.querySelector(".container");
 const background = document.querySelector("#background");
 
+// Seleciona o elemento de loading e os elementos onde os dados meteorológicos serão exibidos.
 const loading = document.querySelector(".fa-solid.fa-spinner.fa-spin.fa-2xl");
 const main = document.querySelector("#weather-data");
 const city_element = document.querySelector("#city");
@@ -16,13 +20,19 @@ const max_element = document.querySelector("#max span");
 const min_element = document.querySelector("#min span");
 const humidity_element = document.querySelector("#umidity span");
 const wind_element = document.querySelector("#wind span");
+
+// Obtém a largura e altura da tela do usuário para buscar uma imagem de fundo adequada.
 const screen_width = screen.width;
 const screen_height = screen.height;
 
+// Função assíncrona para buscar dados meteorológicos e uma imagem de fundo.
 const get_weather_data = async (city) => {
     try {
+        // Esconde o container principal e mostra o ícone de loading.
         container.style.display = "none";
         loading.style.display = "inline-block";
+
+        // Constrói URLs para a API meteorológica e para buscar uma imagem de fundo do Unsplash.
         const api_weather_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
         const [response_weather, response_back] = await Promise.all([
             fetch(api_weather_URL),
@@ -33,12 +43,18 @@ const get_weather_data = async (city) => {
                 )}`
             ),
         ]);
+
+        // Processa a resposta da API meteorológica.
         const data_weather = await response_weather.json();
+
+        // Verifica se ambas as respostas da API estão ok.
         if (!response_weather.ok || !response_back.ok) {
             loading.style.display = "none";
             container.style.display = "inline-block";
             throw new Error("Network response was not ok");
         }
+
+        // Atualiza a UI com os dados meteorológicos e a imagem de fundo.
         loading.style.display = "none";
         container.style.display = "inline-block";
         console.log(data_weather);
@@ -59,6 +75,7 @@ const get_weather_data = async (city) => {
         main.classList.remove("hide");
         city_.textContent = "Check out the climate of a city:";
     } catch (error) {
+        // Em caso de erro, mostra uma imagem de erro e esconde os dados meteorológicos.
         background.style.backgroundImage =
             "url('img/error-computer-lock-screen-9ga1hfyj2zeb892b.jpg')";
         background.style.backgroundRepeat = "no-repeat";
@@ -68,13 +85,19 @@ const get_weather_data = async (city) => {
         city_.textContent = "Please enter a valid city name:";
     }
 };
+
+// Função para iniciar a busca de dados meteorológicos.
 const show_weather_data = (city) => {
     get_weather_data(city);
 };
+
+// Função para obter o valor inserido pelo usuário e chamar a função de busca.
 const input_value = () => {
     const city = city_input.value;
     show_weather_data(city);
 };
+
+// Adiciona event listeners para buscar dados quando o usuário pressiona 'Enter' ou clica no botão de busca.
 city_input.addEventListener("keyup", (e) => {
     e.preventDefault();
     if (event.keyCode === 13) {
